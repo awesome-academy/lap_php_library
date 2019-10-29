46e5a5656913b712fc062bc5af79c5d48524220b
PhamNgocQuy <quypn96@gmail.com>
2019-10-29 09:13:52 +0700

    Create migration

--- database/migrations/2019_10_23_014001_create_authors_table.php
+++ database/migrations/2019_10_23_014001_create_authors_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateAuthorsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,18 +13,12 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('authors', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->string('avatar');
-            $table->integer('phone_number');
             $table->date('birthday');
-            $table->tinyInteger('status');
-            $table->tinyInteger('role_id');
-            $table->rememberToken();
+            $table->string('name');
+            $table->string('avatar');
+            $table->text('description');
             $table->timestamps();
         });
     }
@@ -36,6 +30,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('authors');
     }
 }
