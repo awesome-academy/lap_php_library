931d2e1009b0637bdbdb97ee86f2b8fc56aa1739
PhamNgocQuy <quypn96@gmail.com>
2019-11-06 08:51:12 +0700

    Create factory and seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,18 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        $this->call(UsersTableSeeder::class);
+        $this->call(BooksTableSeeder::class);
+        $this->call(AuthorsTableSeeder::class);
+        $this->call(CategoriesTableSeeder::class);
+        $this->call(CommentsTableSeeder::class);
+        $this->call(FollowsTableSeeder::class);
+        $this->call(LikesTableSeeder::class);
+        $this->call(RatesTableSeeder::class);
+        $this->call(RolesTableSeeder::class);
+        $this->call(PublishersTableSeeder::class);
+        $this->call(BookCategoryTableSeeder::class);
+        $this->call(AuthorBookTableSeeder::class);
+
     }
 }
